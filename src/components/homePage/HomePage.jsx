5d4d0d053a20e4941af6269b1043118fba6ad2ba
AnaMigriauli <anamigriauli1994@gmail.com>
2023-11-04 23:51:53 +0400

    created part of the homepage

--- src/components/homePage/HomePage.jsx
+++ src/components/homePage/HomePage.jsx
@@ -1,16 +1,60 @@
 import ContentCard from "../reusable/ContentCard";
 import img1 from "../../assets/images/home-1.svg";
+import img2 from "../../assets/images/home-2.svg";
 import arrow from "../../assets/images/arrow.svg";
+import arrowBlack from "../../assets/images/blackArrow.svg";
+import style from "./HomePage.module.css";
+import img3 from "../../assets/images/home-3.svg";
+
+const cardData = [
+  {
+    containerstyle: style.textcontainer1,
+    maincontainer: style.maincontainer1,
+    img: img1,
+    arrow: arrow,
+    title: "Create and share your photo stories. ",
+    link: "GET AN INVITE",
+    text: "Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others.",
+  },
+  {
+    containerstyle: style.textcontainer2,
+    maincontainer: style.maincontainer2,
+    img: img2,
+    arrow: arrowBlack,
+    title: "BEAUTIFUL STORIES EVERY TIME",
+    link: "VIEW THE STORIES",
+    text: "We provide design templates to ensure your stories look terrific. Easily add photos, text, embed maps and media from other networks. Then share your story with everyone.",
+  },
+  {
+    containerstyle: style.textcontainer3,
+    maincontainer: style.maincontainer2,
+    img: img3,
+    arrow: arrowBlack,
+    title: "DESIGNED FOR EVERYONE",
+    link: "VIEW THE STORIES",
+    text: "Photosnap can help you create stories that resonate with your audience.  Our tool is designed for photographers of all levels, brands, businesses you name it.",
+  },
+];
 
 const HomePage = () => {
   return (
-    <ContentCard
-      img={img1}
-      arrow={arrow}
-      title="Create and share your photo stories. "
-      link="GET AN INVITE"
-      text="Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others."
-    />
+    <div>
+      {cardData.map((card, i) => (
+        <ContentCard
+          key={i}
+          containerstyle={card.containerstyle}
+          maincontainer={card.maincontainer}
+          headerstyle={style.header}
+          textstyle={style.text}
+          linkstyle={style.link}
+          img={card.img}
+          arrow={card.arrow}
+          title={card.title}
+          link={card.link}
+          text={card.text}
+        />
+      ))}
+    </div>
   );
 };
 
--- src/components/reusable/ContentCard.jsx
+++ src/components/reusable/ContentCard.jsx
@@ -1,11 +1,22 @@
-const ContentCard = ({ img, text, title, link, arrow }) => {
+const ContentCard = ({
+  img,
+  text,
+  title,
+  link,
+  arrow,
+  containerstyle,
+  headerstyle,
+  textstyle,
+  linkstyle,
+  maincontainer,
+}) => {
   return (
-    <div>
+    <div className={maincontainer}>
       <img src={img} />
-      <div>
-        <h1>{title}</h1>
-        <p>{text}</p>
-        <div>
+      <div className={containerstyle}>
+        <h1 className={headerstyle}>{title}</h1>
+        <p className={textstyle}>{text}</p>
+        <div className={linkstyle}>
           <p>{link}</p>
           <img src={arrow} />
         </div>
